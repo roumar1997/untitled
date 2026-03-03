e7743298c43881edcfd3e3078c94aae5ea9bdf44
rouma <roumar1997@gmail.com>
2026-03-03 16:52:07 +0100

    nuevas actualizaciones

---
+++ pruebas/src/Prueba1.java
@@ -0,0 +1,19 @@
+
+public class Prueba1 {
+
+
+    public static void main(String[] args ){
+        System.out.println("hola mundo ");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+    }
+}
+
+
+
+
+
